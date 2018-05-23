658cbf0dc49a5dcecab6fd69ba1d9d7f477d0b80
xiaoxinwu <xxw.show@qq.com>
2018-05-23 17:37:36 +0800

    update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8090';
+axios.defaults.baseURL = 'http://127.0.0.1:8090'; //dev
+axios.defaults.baseURL = 'http://shawbosen/node'; //prod
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 
